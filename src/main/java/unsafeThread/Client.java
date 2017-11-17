2fe00bedafefcb852a8e7c25d803b95dac1b32a5
demofang <xhf6688@126.com>
2017-11-17 22:37:03 +0800

    thread not safe example

---
+++ src/main/java/unsafeThread/Client.java
@@ -0,0 +1,16 @@
+package unsafeThread;
+
+/**
+ * test for thread
+ * Created by fang on 2017/11/17.
+ */
+public class Client {
+    public static void main(String[] args) {
+        UnsafeSequence unsafeSequence = new UnsafeSequence();
+        for(int i=0;i<10000;i++){
+            Thread thread = new Thread(unsafeSequence);
+            thread.start();
+        }
+
+    }
+}
---
+++ src/main/java/unsafeThread/UnsafeSequence.java
@@ -0,0 +1,16 @@
+package unsafeThread;
+//import net.jcip.annotations.*;
+/**
+ * Created by fang on 2017/11/17.
+ */
+//@NotThreadSafe
+public class UnsafeSequence implements Runnable{
+    private int value;
+    public synchronized int getNext(){
+        return value++;
+    }
+    public void run(){
+        System.out.println(getNext());
+    }
+
+}
