15522402a04e6d5dede715969f62825a80a188c4
demofang <xhf6688@126.com>
2017-11-17 22:50:28 +0800

    modify name

--- src/main/java/unsafeThread/UnsafeThreadClient.java
+++ src/main/java/unsafeThread/UnsafeThreadClient.java
@@ -4,7 +4,7 @@ package unsafeThread;
  * test for thread
  * Created by fang on 2017/11/17.
  */
-public class Client {
+public class UnsafeThreadClient {
     public static void main(String[] args) {
         UnsafeSequence unsafeSequence = new UnsafeSequence();
         for(int i=0;i<10000;i++){
