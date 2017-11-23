94b67f6c659cfef66d178495b027e0c2b13baa20
demofang <xhf6688@126.com>
2017-11-24 01:40:07 +0800

    实例方法中的同步块,synchronized作用于静态方法和费静态方法上示例

--- src/main/java/synchironized_ex/InstanceSynchronized.java
+++ src/main/java/synchironized_ex/InstanceSynchronized.java
@@ -5,7 +5,7 @@ package synchironized_ex;
  * 类的每个实例同步示例.
  */
 public class InstanceSynchronized {
-    public synchronized void instanceMethod(){
+    public static synchronized void instanceMethod(){//加static和不加static的区别,锁类和锁对象.
         System.out.println("执行实例同步方法开始......");
          try{
             Thread.sleep(1000);
