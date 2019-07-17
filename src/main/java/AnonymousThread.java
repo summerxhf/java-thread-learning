77530f33e964901f90160f1b0a23d0e88ee37453
xhaifang <xinghaifang@kingsoft.com>
2019-07-17 15:55:45 +0800

    使用Callable + FutureTask 或者ExecutorService + Future + Callable实现带返回值的任务

--- src/main/java/AnonymousThread.java
+++ src/main/java/AnonymousThread.java
@@ -1,4 +1,4 @@
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -7,7 +7,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * 匿名创建线程的几种方式
  */
 public class AnonymousThread {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         //方式一:使用匿名内部类创建线程的子类对象;
         Thread thread = new Thread() {
             public void run() {
@@ -53,8 +53,58 @@ public class AnonymousThread {
                 System.out.println("使用匿名内部类方式,创建线程的匿名对象");
             }
         }).start();
+
+        //方式六: lambda形式使用线程
+        new Thread(()->{
+            System.out.println("lambda形式使用线程");
+        }).start();
+
+
+        //方式七:待返回值形式使用Callable  futuretask获取带有返回值的任务;
+        Callable<Integer> call = ()->{
+            System.out.println("带有返回值的线程执行");
+            return 11;
+        };
+
+        //将任务封装成FutureTask;
+        FutureTask<Integer> futureTask = new FutureTask<>(call);
+
+        //开启线程,执行线程任务;
+        new Thread(futureTask).start();
+        System.out.println("输入带有返回值的结果: " +futureTask.get());
+
+        //方式八: 使用ExecutorService Callable Future实现带有返回结果的多线程
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Callable<Integer> callable = new MyCallable("吃饭任务");
+        Future future = executorService.submit(callable);
+        System.out.println("ExecutorService Callable Future返回结果-------------"+future.get());
+
+        //关闭线程池;
+        executorService.shutdown();
     }
 
 //    CopyOnWriteArrayList<Integer> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
 
-}
+
+ }
+
+
+ class MyCallable implements Callable<Integer>{
+    private String taskName;
+
+    public MyCallable(String taskName){
+        this.taskName = taskName;
+    }
+
+     @Override
+     public Integer call() {
+         System.out.println("任务执行中----------------------");
+         try {
+             Thread.sleep(1000);
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+         System.out.println("任务执行完毕-----------------------");
+         return 2222;
+     }
+ }
